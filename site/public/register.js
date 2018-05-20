ad4cf34908c3a053ef5a4ac2372949215acc1883
David <dz15376@my.bristol.ac.uk>
2018-05-20 16:48:29 +0100

    added the missing file

---
+++ site/public/register.js
@@ -0,0 +1,39 @@
+"use strict"
+
+addEventListener("load", start);
+
+function start() {
+  var cookies = document.cookie;
+  var cookieArray = cookies.split("; ");
+  getUser();
+  succeedOrFail();
+}
+
+function getUser() {
+  var q = new XMLHttpRequest();
+  q.onreadystatechange = receiveUser;
+  q.open("GET", "/getcurrentuser", true);
+  q.send();
+}
+
+function receiveUser() {
+  if (this.readyState != 4) return;
+  if(this.responseText != "data not found") {
+    var user = JSON.parse(this.responseText);
+    document.getElementById("useruid").value = user.uId;
+    document.getElementById("username").value = user.name;
+    document.getElementById("useravatar").value = user.avatar;
+    document.getElementById("useremail").value = user.email;
+  }
+  else {
+    removePostingTools();
+  }
+  addHeader();
+}
+
+function succeedOrFail() {
+  var url = window.location.href;
+  var value = url.split("#")[1];
+  if(value == null) return;
+  if(value == "failed") console.log("failed");
+}
